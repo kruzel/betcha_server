7bea50399a8e3d4252a3ec775a90457e2298f763
ofer kruzel <okruzel@gmail.com>
2012-07-31 00:18:42 +0300

    GET friends.json - return full friends details

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -4,13 +4,13 @@ class FriendsController < ApplicationController
   # GET /friends/show_for_user
   # GET /friends/show_for_user.json
   def show_for_user 
-    friends_ids = Friends.find_all_by_user_id (current_user.id)
+    friends_ids = Friend.find_all_by_user_id (current_user.id)
           
     @friends = Array.new
     friends_ids.each do |friend|
-      #user = User.find(friend.friend)
+        user = User.find(friend.friend_id)
         #@friend_list << user unless (user.nil?)
-        @friends << friend
+        @friends << user
     end unless (friends_ids.nil?)
       
     respond_to do |format|
