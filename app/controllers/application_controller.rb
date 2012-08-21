c2d79e00015e7bbdf7e20588cbb835beea77820f
ofer kruzel <okruzel@gmail.com>
2012-08-21 23:13:48 +0300

    support access_token in HTTP header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  prepend_before_filter :get_api_key
+
+  private
+  def get_api_key
+    if api_key = params[:api_key].blank? && request.headers["X-API-KEY"]
+      params[:api_key] = api_key
+    end
+  end
+
+  
   def after_sign_in_path_for(resource)
       return user_path(resource) || request.env['omniauth.origin'] || stored_location_for(resource) || root_path
   end
