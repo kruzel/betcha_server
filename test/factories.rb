74aa09310841ea366b33adbfd39c182e3578ea97
ofer kruzel <okruzel@gmail.com>
2012-10-01 12:34:54 +0200

    tests fix

--- test/factories.rb
+++ test/factories.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :user do
+  factory :user do       |user|
     user.full_name              "ofer"
     user.email                  "ofer@gmail.com"
     user.password               "foobar"
